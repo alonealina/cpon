6495fcc9046267863d8f205b548407f6b878651b
bluesky2008 <william_billl2008@yahoo.co.jp>
2021-12-08 10:33:46 +0900

    お知らせのデザイン修正

--- resources/views/notice/show.blade.php
+++ resources/views/notice/show.blade.php
@@ -5,7 +5,7 @@
 <p class="cpon_notice">Cポンポータルからのお知らせ</p>
 
 <div class="notice_show">
-    <div class="notice_date">{{ $notice->notice_date }}</div>
+    <div class="notice_content_date">{{ $notice->notice_date }}</div>
     <div class="notice_title_big">{{ $notice->title }}</div>
     <div class="notice_content">{{ $notice->content }}</div>
 </div>
