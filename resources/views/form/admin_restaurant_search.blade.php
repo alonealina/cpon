f1a3c0466e505063bd5b19f5811ab3acdc90c9c4
bluesky2008 <william_billl2008@yahoo.co.jp>
2021-12-21 16:07:03 +0900

    年部分の値範囲　2021～2050年に決定

--- resources/views/form/admin_restaurant_search.blade.php
+++ resources/views/form/admin_restaurant_search.blade.php
@@ -77,21 +77,21 @@
         <div class="filter_flex_right">
             <div class="filter_name">登録日</div>
             <div class="filter_created">
-                {!! Form::selectRange('created_year_before', 2020, 2040, $created_year_before, ['placeholder'=>'年']) !!}
+                {!! Form::selectRange('created_year_before', 2021, 2050, $created_year_before, ['placeholder'=>'年']) !!}
                 {!! Form::selectRange('created_month_before', 1, 12, $created_month_before, ['placeholder'=>'月']) !!}
                 {!! Form::selectRange('created_day_before', 1, 31, $created_day_before, ['placeholder'=>'日']) !!}
                 　～　
-                {!! Form::selectRange('created_year_after', 2020, 2040, $created_year_after, ['placeholder'=>'年']) !!}
+                {!! Form::selectRange('created_year_after', 2021, 2050, $created_year_after, ['placeholder'=>'年']) !!}
                 {!! Form::selectRange('created_month_after', 1, 12, $created_month_after, ['placeholder'=>'月']) !!}
                 {!! Form::selectRange('created_day_after', 1, 31, $created_day_after, ['placeholder'=>'日']) !!}
             </div>
             <div class="filter_name">更新日</div>
             <div class="filter_updated">
-                {!! Form::selectRange('updated_year_before', 2020, 2040, $updated_year_before, ['placeholder'=>'年']) !!}
+                {!! Form::selectRange('updated_year_before', 2021, 2050, $updated_year_before, ['placeholder'=>'年']) !!}
                 {!! Form::selectRange('updated_month_before', 1, 12, $updated_month_before, ['placeholder'=>'月']) !!}
                 {!! Form::selectRange('updated_day_before', 1, 31, $updated_day_before, ['placeholder'=>'日']) !!}
                 　～　
-                {!! Form::selectRange('updated_year_after', 2020, 2040, $updated_year_after, ['placeholder'=>'年']) !!}
+                {!! Form::selectRange('updated_year_after', 2021, 2050, $updated_year_after, ['placeholder'=>'年']) !!}
                 {!! Form::selectRange('updated_month_after', 1, 12, $updated_month_after, ['placeholder'=>'月']) !!}
                 {!! Form::selectRange('updated_day_after', 1, 31, $updated_day_after, ['placeholder'=>'日']) !!}
             </div>
