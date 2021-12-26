f83e42a9936ff3607310a0641682fd421724dc11
bluesky2008 <william_billl2008@yahoo.co.jp>
2021-12-26 18:36:37 +0900

    バナー20件の場合登録ボタンを非表示

--- resources/views/admin/banner_list.blade.php
+++ resources/views/admin/banner_list.blade.php
@@ -4,9 +4,11 @@
 
 <nav class="navbar admin_header">
     <div class="content_title">ヘッダーバナー画像管理</div>
+    @if($banners->count() < 20)
     <div class="button_red_admin">
         <a href="{{ route('admin.banner_regist') }}">バナー登録</a>
     </div>
+    @endif
 </nav>
 <div class="admin_content">
     @include('admin.item.banner_list')
