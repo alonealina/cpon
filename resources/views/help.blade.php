a541f2795d61455431a883677efc49fa4b5f3a38
bluesky2008 <william_billl2008@yahoo.co.jp>
2021-12-27 14:12:41 +0900

    ヘルプ更新

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -5,11 +5,11 @@
 <p class="footer_item_title">ヘルプ・お問い合わせ</p>
 <div class="help_content">
     <div class="help_text">
-    CポンやCポンモール(本サイト)全体に関するご質問等がございましたら、下記QRコードを読み取って<br>
-    LINE公式アカウントを登録して頂き、お問合せをお願い致します。専門スタッフからご回答をさせて頂きます。
+    Cポンお店ナビ(本サイト)に関するご質問等がございましたら、<br>
+    下記QRコードを読み取ってLINE公式アカウントを登録して頂き、お問い合わせをお願い致します。
     </div>
     <div class="line_url">
-        <a href="#">LINE公式アカウント</a>
+        <a href="https://liff.line.me/1656420340-x4VZMLXg/landing?follow=%40010okbuc&lp=PCFYfV&liff_id=1656420340-x4VZMLXg">LINE公式アカウント</a>
     </div>
     <img src="{{ asset('img/qrcode.png') }}" class="qr_code" alt="">
 </div>
@@ -21,11 +21,11 @@
     <p class="footer_item_title_ipad">ヘルプ・お問い合わせ</p>
     <div class="help_content">
         <div class="help_text">
-        CポンやCポンモール(本サイト)全体に関するご質問等がございましたら、下記QRコードを読み取って<br>
-        LINE公式アカウントを登録して頂き、お問合せをお願い致します。専門スタッフからご回答をさせて頂きます。
+        Cポンお店ナビ(本サイト)に関するご質問等がございましたら、<br>
+        下記QRコードを読み取ってLINE公式アカウントを登録して頂き、お問い合わせをお願い致します。
         </div>
         <div class="line_url">
-            <a href="#">LINE公式アカウント</a>
+            <a href="https://liff.line.me/1656420340-x4VZMLXg/landing?follow=%40010okbuc&lp=PCFYfV&liff_id=1656420340-x4VZMLXg">LINE公式アカウント</a>
         </div>
         <img src="{{ asset('img/qrcode.png') }}" class="qr_code" alt="">
     </div>
@@ -42,10 +42,10 @@
 <p class="footer_item_title_sp">ヘルプ・お問い合わせ</p>
 <div class="help_content">
     <div class="help_text">
-    CポンやCポンモール(本サイト)全体に関するご質問等がございましたら、下記QRコードを読み取ってLINE公式アカウントを登録して頂き、お問合せをお願い致します。専門スタッフからご回答をさせて頂きます。
+    Cポンお店ナビ(本サイト)に関するご質問等がございましたら、下記QRコードを読み取ってLINE公式アカウントを登録して頂き、お問い合わせをお願い致します。
     </div>
     <div class="line_url">
-        <a href="#">LINE公式アカウント</a>
+        <a href="https://liff.line.me/1656420340-x4VZMLXg/landing?follow=%40010okbuc&lp=PCFYfV&liff_id=1656420340-x4VZMLXg">LINE公式アカウント</a>
     </div>
     <img src="{{ asset('img/qrcode.png') }}" class="qr_code_sp" alt="">
 </div>
