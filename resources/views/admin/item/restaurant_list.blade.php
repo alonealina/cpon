9f9d4cb407c7a63aa8f77966f8e5cdd9e883d9a6
bluesky2008 <william_billl2008@yahoo.co.jp>
2021-12-27 22:34:18 +0900

    CSS修正

--- resources/views/admin/item/restaurant_list.blade.php
+++ resources/views/admin/item/restaurant_list.blade.php
@@ -97,8 +97,8 @@
             <div class="restaurant_list_button_blue">
                 <a href="restaurant_edit/{{ $restaurant->id }}">編集</a>
             </div>
-            <div class="restaurant_list_button_blue">
-                <a href="{{ route('admin.menu_list', ['id' => $restaurant->id]) }}">メニュー<br>詳細</a>
+            <div class="restaurant_list_detail_button_blue">
+                <a href="{{ route('admin.menu_list', ['id' => $restaurant->id]) }}">メニュー<br>一覧</a>
             </div>
             <div class="restaurant_list_button_red">
                 <a href="restaurant_delete/{{ $restaurant->id }}" onclick="return confirm('本当に削除しますか？')">削除</a>
