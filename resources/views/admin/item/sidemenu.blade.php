c9f29926b657e84511f16120123834efde297987
bluesky2008 <william_billl2008@yahoo.co.jp>
2021-12-28 14:20:55 +0900

    ログイン画面CSS実装　メニューバーとヘッダーの画像実装

--- resources/views/admin/item/sidemenu.blade.php
+++ resources/views/admin/item/sidemenu.blade.php
@@ -1,19 +1,54 @@
 <div class="flexible-list-sidebar">
     @if(session('type') == 'operation')
-    <div class="sidemenu_name"><a href="{{ route('admin.restaurant_list') }}">店舗管理</a></div>
+    <a href="{{ route('admin.restaurant_list') }}"><div class="sidemenu_name">
+        <div class="sidemenu_img">
+        <img src="{{ asset('img/unei_home.png') }}" class="" alt="">
+        </div>
+        店舗管理
+    </div></a>
     <hr>
-    <div class="sidemenu_name"><a href="{{ route('admin.notice_list') }}">お知らせ管理</a></div>
+    <a href="{{ route('admin.notice_list') }}"><div class="sidemenu_name">
+        <div class="sidemenu_img">
+        <img src="{{ asset('img/unei_mail.png') }}" class="" alt="">
+        </div>
+        お知らせ管理
+    </div></a>
     <hr>
-    <div class="sidemenu_name"><a href="{{ route('admin.banner_list') }}">画像管理</a></div>
+    <a href="{{ route('admin.banner_list') }}"><div class="sidemenu_name">
+        <div class="sidemenu_img">
+        <img src="{{ asset('img/unei_img.png') }}" class="" alt="">
+        </div>
+        画像管理
+    </div></a>
     <hr>
-    <div class="sidemenu_name"><a href="{{ route('admin.setting_list') }}">各種設定</a></div>
+    <a href="{{ route('admin.setting_list') }}"><div class="sidemenu_name">
+        <div class="sidemenu_img">
+        <img src="{{ asset('img/unei_sistem.png') }}" class="" alt="">
+        </div>
+        各種設定
+    </div></a>
     <hr>
     @else
-    <div class="sidemenu_name"><a href="{{ route('admin.restaurant_edit', ['id' => session('id')]) }}">店舗情報編集</a></div>
+    <a href="{{ route('admin.restaurant_edit', ['id' => session('id')]) }}"><div class="sidemenu_name">
+        <div class="sidemenu_img">
+        <img src="{{ asset('img/tenpo_home.png') }}" class="" alt="">
+        </div>
+        店舗情報編集
+    </div></a>
     <hr>
-    <div class="sidemenu_name"><a href="{{ route('admin.menu_list', ['id' => session('id')]) }}">メニュー管理</a></div>
+    <a href="{{ route('admin.menu_list', ['id' => session('id')]) }}"><div class="sidemenu_name">
+        <div class="sidemenu_img">
+        <img src="{{ asset('img/tenpo_menu.png') }}" class="" alt="">
+        </div>
+        メニュー管理
+    </div></a>
     <hr>
-    <div class="sidemenu_name"><a href="{{ route('admin.comment_list', ['id' => session('id')]) }}">クチコミ管理</a></div>
+    <a href="{{ route('admin.comment_list', ['id' => session('id')]) }}"><div class="sidemenu_name">
+        <div class="sidemenu_img">
+        <img src="{{ asset('img/tenpo_coment.png') }}" class="" alt="">
+        </div>
+        クチコミ管理
+    </div></a>
     <hr>
     @endif
 </div>
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,32 +3,32 @@
 @section('title', 'ログイン')
  
 @section('content')
-<p></p>
-<h1>ログイン</h1>
-<p></p>
- 
-{{-- エラーメッセージ --}}
-@if (isset($login_error))
-  <div id="error_explanation" class="text-danger">
-    <ul>
-      <li>メールアドレスまたはパスワードが一致しません。</li>
-    </ul>
-  </div>
-@endif
-<p></p>
- 
-{{-- フォーム --}}
-<form action="{{ url('admin_login') }}" method="post">
-  @csrf  
-  <div class="form-group">
-    <label for="user_email">ログインID</label>
-    <input type="text" class="form-control" id="user_email" name="login_id">
-  </div>     
-  <div class="form-group">
-    <label for="user_password">パスワード</label>
-    <input type="password" class="form-control" id="user_password" name="password">
-  </div>     
-  <input type="submit" value="ログインする" class="btn btn-primary">  
-</form>  
-<p><br></p>
+
+<div class="login_form">
+  <img src="{{ asset('img/logo.png') }}" class="login_logo" alt="">
+  {{-- エラーメッセージ --}}
+  @if (isset($login_error))
+    <div id="error_explanation" class="text-danger">
+      <ul>
+        <li>メールアドレスまたはパスワードが一致しません。</li>
+      </ul>
+    </div>
+  @endif
+  
+  {{-- フォーム --}}
+  <form action="{{ url('admin_login') }}" method="post">
+    @csrf  
+    <div class="form-group">
+      <div class="login_form_name">ログインID</div>
+      <input type="text" class="form-control" id="user_email" name="login_id">
+    </div>     
+    <div class="form-group">
+    <div class="login_form_name">パスワード</div>
+      <input type="password" class="form-control" id="user_password" name="password">
+    </div>     
+    <input type="submit" value="ログイン" class="btn login_button">  
+  </form>  
+</div>
+<div class="copyright">Copyright © 2021 KOC・JAPAN All Rights Reserved.</div>
+
 @endsection
