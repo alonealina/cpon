ff8cbad8e68fadfcae2acd5b2c61caacb4086e9e
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-01-06 17:14:15 +0900

    デザイン修正

--- resources/views/form/filter_search_sp.blade.php
+++ resources/views/form/filter_search_sp.blade.php
@@ -1,6 +1,6 @@
 <div class="flexible-list-sidebar_sp">
     <form id="form" name="filter_form_ipad" action="{{ route('filter') }}" method="get">
-        <div class="filter_form_title_ipad">絞り込み</div>
+        <div class="filter_form_title_ipad">絞り込み　▼</div>
         <input type="checkbox" class="filter_open_checkbox" id="sp01"><label for="sp01" class="filter_open_label_sp" style="border:none;"></label>
         <div class="filter_form_sp">
             <hr>
